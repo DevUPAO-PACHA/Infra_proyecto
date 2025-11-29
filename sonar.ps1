714f20da48007ae408282af8fca095b81e3c6f20
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-11-29 13:29:49 -0500

    feat: add sonar services and script

---
+++ sonar.ps1
@@ -0,0 +1,4 @@
+& "C:\sonar\sonar-scanner-7.3.0.5189-windows-x64\bin\sonar-scanner.bat" `
+  -D"sonar.projectKey=infra-proyecto-sonar" `
+  -D"sonar.sources=." `
+  -D"sonar.host.url=http://localhost:9000"
