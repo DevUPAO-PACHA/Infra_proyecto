2cc679b503a3b28d92d6c7caf0c87f6c3b76759c
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-12-02 16:55:10 -0500

    chore(alb.tf): clean code

--- alb.tf
+++ alb.tf
@@ -1,4 +1,4 @@
-# --- 1. Application Load Balancer (ALB) ---
+
 resource "aws_lb" "main" {
   name               = "mi-app-alb"
   internal           = false # Es público
@@ -11,9 +11,6 @@ resource "aws_lb" "main" {
   }
 }
 
-# --- 2. Target Group (TG) ---
-# El ALB no envía tráfico a Fargate, lo envía a un "grupo".
-# Fargate se registra en este grupo.
 resource "aws_lb_target_group" "api" {
   name        = "${var.app_name}-tg"
   port        = var.app_port
@@ -32,8 +29,6 @@ resource "aws_lb_target_group" "api" {
   }
 }
 
-# --- 3. Listener del ALB ---
-# Escucha en el puerto 80 (HTTP) y reenvía el tráfico al Target Group.
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
@@ -44,13 +39,3 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.api.arn 
   }
 }
-/* Esto es solo por si fuera con dominio real pero no hay pue :c
-resource "aws_lb_listener" "https" { 
-  load_balancer_arn = aws_lb.main.arn 
-  port = 443 
-  protocol = "HTTPS"
-  default_action { 
-    type = "forward" 
-    target_group_arn = aws_lb_target_group.api.arn 
-  } 
-}*/
