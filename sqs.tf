444096673fc9c4f490be8ea1837201e33f69477a
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-12-02 16:50:27 -0500

    chore(sqs.tf): clean code

--- sqs.tf
+++ sqs.tf
@@ -1,5 +1,4 @@
-# --- 1. Dead Letter Queue (DLQ) ---
-# Aquí caen los mensajes que fallan repetidamente.
+
 resource "aws_sqs_queue" "reservas_dlq" {
   name = "reservas-dlq"
 
@@ -8,12 +7,9 @@ resource "aws_sqs_queue" "reservas_dlq" {
   }
 }
 
-# --- 2. Cola Principal de Reservas ---
 resource "aws_sqs_queue" "reservas_queue" {
   name = "reservas-queue"
 
-  # Política de "Redrive": Después de 3 intentos fallidos (maxReceiveCounts),
-  # mueve el mensaje a la DLQ.
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.reservas_dlq.arn
     maxReceiveCount    = 3
