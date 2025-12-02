baefd17ad848d6010a05d6e7dbad0c433ea81b0a
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-12-02 16:52:57 -0500

    chore(database.tf): clean code

--- database.tf
+++ database.tf
@@ -1,11 +1,10 @@
-# --- 1. Generar una contraseña aleatoria y segura ---
+
 resource "random_password" "db" {
   length           = 16
   special          = true
   override_special = "_#"
 }
 
-# --- 2. Crear el Secreto en AWS Secrets Manager ---
 resource "aws_secretsmanager_secret" "db" {
   name = "${var.app_name}/db-password"
 
@@ -14,13 +13,11 @@ resource "aws_secretsmanager_secret" "db" {
   }
 }
 
-# --- 3. Poblar el Secreto ---
 resource "aws_secretsmanager_secret_version" "db" {
   secret_id     = aws_secretsmanager_secret.db.id
   secret_string = random_password.db.result
 }
 
-# --- 4. Crear el Cluster Aurora Serverless v2 ---
 resource "aws_rds_cluster" "aurora" {
   cluster_identifier = "mi-app-aurora-cluster"
   engine             = "aurora-mysql"
@@ -34,7 +31,6 @@ resource "aws_rds_cluster" "aurora" {
   skip_final_snapshot    = true
   storage_encrypted      = true
 
-  # ACTIVAR SERVERLESS V2
   serverlessv2_scaling_configuration {
     min_capacity = 0.5
     max_capacity = 4
@@ -49,13 +45,11 @@ resource "aws_rds_cluster" "aurora" {
   }
 }
 
-# --- 5. Crear las Instancias del Cluster ---
 resource "aws_rds_cluster_instance" "aurora" {
   count              = 2
   cluster_identifier = aws_rds_cluster.aurora.id
   identifier         = "mi-app-aurora-instance-${count.index}"
 
-  # Instancia compatible con Serverless v2
   instance_class = "db.serverless"
 
   engine         = aws_rds_cluster.aurora.engine
