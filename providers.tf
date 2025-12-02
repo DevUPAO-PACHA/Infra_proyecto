bef4d70ba4d6d799cc626fee8104dd8692fa7b94
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-12-02 12:06:49 -0500

    refactor: providers's structure was improved

--- providers.tf
+++ providers.tf
@@ -3,29 +3,24 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.23.0"
+      version = "~> 6.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.7.2"
+      version = "~> 3.5"
     }
   }
 
-  # NOTA: Más adelante, aquí configuraremos el "Backend Remoto"
-  # (S3 + DynamoDB) que discutimos, para guardar el estado .tfstate
-  # de forma segura. Por ahora, para esta primera parte,
-  # se guardará localmente.
   backend "s3" {
-    bucket = "mi-app-tfstate-bucket-695100305629" # El nombre de tu bucket
-    key = "global/terraform.tfstate"           # La ruta donde se guardará el estado
-    region = "us-east-1"                          # La región donde creaste el bucket
-    dynamodb_table = "mi-app-terraform-lock"            # El nombre de tu tabla
+    bucket = "mi-app-tfstate-bucket-695100305629"   # El nombre de tu bucket
+    key = "global/terraform.tfstate"                # La ruta donde se guardará el estado
+    region = "us-east-1"                            # La región donde creaste el bucket
+    dynamodb_table = "mi-app-terraform-lock"        # El nombre de tu tabla
     encrypt = true
   }
 
 }
 
-# Configuración del proveedor de AWS
 provider "aws" {
   region = var.aws_region
 }
