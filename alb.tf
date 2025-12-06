1806efbb806283fb6069a727ffe5685a56d0fee5
Angel <113852397+AngelOncoy@users.noreply.github.com>
2025-12-06 00:54:40 -0500

    fix: http to https

--- alb.tf
+++ alb.tf
@@ -16,14 +16,14 @@ resource "aws_lb" "main" {
 resource "aws_lb_target_group" "api" {
   name        = "${var.app_name}-${var.environment}-tg"
   port        = var.app_port
-  protocol    = "HTTP"
+  protocol    = "HTTPS"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
 
   health_check {
     enabled  = true
     path     = "/actuator/health"
-    protocol = "HTTP"
+    protocol = "HTTPS"
   }
 
   tags = {
@@ -34,7 +34,7 @@ resource "aws_lb_target_group" "api" {
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
-  protocol          = "HTTP"
+  protocol          = "HTTPS"
 
   default_action {
     type             = "forward"
